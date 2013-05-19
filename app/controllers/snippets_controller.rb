4128a89d66584d90f75e94fc6602dbb8b866f56b
Vyacheslav Loginov <vyacheslav.loginov@nasctech.com>
2013-05-19 03:34:55 +0300

    add more

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -1,4 +1,12 @@
 class SnippetsController < InheritedResources::Base
+  def index
+    @snippets = if params[:more]
+      Snippet.where("id > #{params[:more]}")
+    else
+      Snippet.all
+    end
+  end
+
   def update
     update! { snippets_path }
   end
